0f9590dec7832e11f39b136b6c6c88ed1fe93b1e
zz <xzrunner@gmail.com>
2020-05-29 07:32:01 +0800

    up test

--- test/glsl_test.cpp
+++ test/glsl_test.cpp
@@ -15,6 +15,16 @@ TEST_CASE("global_var")
 
 int val;
 uniform vec2 val2;
+uniform vec3 lightPositions[4];
+
+int val2 = 1;
+void main()
+{
+    int val2 = 2;
+    val2 = 1;
+    float val3 = 1e-5;
+    val3 = lightPositions[1].x;
+}
 
 )";
 
@@ -27,8 +37,16 @@ uniform vec2 val2;
 
     REQUIRE(ss.str() == R"(
 int val;
-
 uniform vec2 val2;
+uniform vec3 lightPositions[4];
+int val2 = 1;
+void main()
+{
+    int val2 = 2;
+    val2 = 1;
+    float val3 = 1e-05;
+    val3 = lightPositions[1].x;
+}
 
 )"+1);
 
@@ -57,7 +75,7 @@ float ellipse(vec2 uv, float width, float height)
     REQUIRE(ss.str() == R"(
 float ellipse(vec2 uv, float width, float height)
 {
-    float(d) = length((((uv * 2) - 1) / vec2(width, height)));
+    float d = length((((uv * 2) - 1) / vec2(width, height)));
     return clamp(((1 - d) / fwidth(d)), 0.0, 1.0);
 }
 
@@ -104,16 +122,16 @@ vec3 hue(vec3 rgb, float offset)
     REQUIRE(ss.str() == R"(
 vec3 hue(vec3 rgb, float offset)
 {
-    vec4(K) = vec4(0.0, ((- 1.0) / 3.0), (2.0 / 3.0), (- 1.0));
-    vec4(P) = mix(vec4(rgb.bg, K.wz), vec4(rgb.gb, K.xy), step(rgb.b, rgb.g));
-    vec4(Q) = mix(vec4(P.xyw, rgb.r), vec4(rgb.r, P.yzx), step(P.x, rgb.r));
-    float(D) = (Q.x - min(Q.w, Q.y));
-    float(E) = 1e-10.0;
-    vec3(hsv) = vec3(abs((Q.z + ((Q.w - Q.y) / ((6.0 * D) + E)))), (D / (Q.x + E)), Q.x);
-    float(hue) = (hsv.x + offset);
+    vec4 K = vec4(0.0, ((- 1.0) / 3.0), (2.0 / 3.0), (- 1.0));
+    vec4 P = mix(vec4(rgb.bg, K.wz), vec4(rgb.gb, K.xy), step(rgb.b, rgb.g));
+    vec4 Q = mix(vec4(P.xyw, rgb.r), vec4(rgb.r, P.yzx), step(P.x, rgb.r));
+    float D = (Q.x - min(Q.w, Q.y));
+    float E = 1e-10;
+    vec3 hsv = vec3(abs((Q.z + ((Q.w - Q.y) / ((6.0 * D) + E)))), (D / (Q.x + E)), Q.x);
+    float hue = (hsv.x + offset);
     hsv.x = ((hue < 0) ? (hue + 1) : ((hue > 1) ? (hue - 1) : hue));
-    vec4(K2) = vec4(1.0, (2.0 / 3.0), (1.0 / 3.0), 3.0);
-    vec3(P2) = abs(((fract((hsv.xxx + K2.xyz)) * 6.0) - K2.www));
+    vec4 K2 = vec4(1.0, (2.0 / 3.0), (1.0 / 3.0), 3.0);
+    vec3 P2 = abs(((fract((hsv.xxx + K2.xyz)) * 6.0) - K2.www));
     return (hsv.z * mix(K2.xxx, clamp((P2 - K2.xxx), 0.0, 1.0), hsv.y));
 }
 
@@ -182,23 +200,23 @@ vec3 white_balance(vec3 rgb, float temperature, float tint)
     REQUIRE(ss.str() == R"(
 vec3 white_balance(vec3 rgb, float temperature, float tint)
 {
-    float(t1) = ((temperature * 10) / 6);
-    float(t2) = ((tint * 10) / 6);
-    float(x) = (0.31271 - (t1 * ((t1 < 0) ? 0.1 : 0.05)));
-    float(standardIlluminantY) = (((2.87 * x) - ((3 * x) * x)) - 0.275095);
-    float(y) = (standardIlluminantY + (t2 * 0.05));
-    vec3(w1) = vec3(0.949237, 1.03542, 1.08728);
-    float(Y) = 1;
-    float(X) = ((Y * x) / y);
-    float(Z) = ((Y * ((1 - x) - y)) / y);
-    float(L) = (((0.7328 * X) + (0.4296 * Y)) - (0.1624 * Z));
-    float(M) = ((((- 0.7036) * X) + (1.6975 * Y)) + (0.0061 * Z));
-    float(S) = (((0.003 * X) + (0.0136 * Y)) + (0.9834 * Z));
-    vec3(w2) = vec3(L, M, S);
-    vec3(balance) = vec3((w1.x / w2.x), (w1.y / w2.y), (w1.z / w2.z));
-    mat3(LIN_2_LMS_MAT) = mat3(0.390405, 0.549941, 0.00892632, 0.0708416, 0.963172, 0.00135775, 0.0231082, 0.128021, 0.936245);
-    mat3(LMS_2_LIN_MAT) = mat3(2.85847, (- 1.62879), (- 0.024891), (- 0.210182), 1.1582, 0.000324281, (- 0.041812), (- 0.118169), 1.06867);
-    vec3(lms) = (LIN_2_LMS_MAT * rgb);
+    float t1 = ((temperature * 10) / 6);
+    float t2 = ((tint * 10) / 6);
+    float x = (0.31271 - (t1 * ((t1 < 0) ? 0.1 : 0.05)));
+    float standardIlluminantY = (((2.87 * x) - ((3 * x) * x)) - 0.275095);
+    float y = (standardIlluminantY + (t2 * 0.05));
+    vec3 w1 = vec3(0.949237, 1.03542, 1.08728);
+    float Y = 1;
+    float X = ((Y * x) / y);
+    float Z = ((Y * ((1 - x) - y)) / y);
+    float L = (((0.7328 * X) + (0.4296 * Y)) - (0.1624 * Z));
+    float M = ((((- 0.7036) * X) + (1.6975 * Y)) + (0.0061 * Z));
+    float S = (((0.003 * X) + (0.0136 * Y)) + (0.9834 * Z));
+    vec3 w2 = vec3(L, M, S);
+    vec3 balance = vec3((w1.x / w2.x), (w1.y / w2.y), (w1.z / w2.z));
+    mat3 LIN_2_LMS_MAT = mat3(0.390405, 0.549941, 0.00892632, 0.0708416, 0.963172, 0.00135775, 0.0231082, 0.128021, 0.936245);
+    mat3 LMS_2_LIN_MAT = mat3(2.85847, (- 1.62879), (- 0.024891), (- 0.210182), 1.1582, 0.000324281, (- 0.041812), (- 0.118169), 1.06867);
+    vec3 lms = (LIN_2_LMS_MAT * rgb);
     lms *= balance;
     return (LMS_2_LIN_MAT * lms);
 }
@@ -312,7 +330,7 @@ float rounded_polygon(vec2 uv, float width, float height, float sides, float rou
     REQUIRE(ss.str() == R"(
 float atan2(float y, float x)
 {
-    bool(s) = (abs(x) > abs(y));
+    bool s = (abs(x) > abs(y));
     return mix(((3.14159 / 2.0) - atan(x, y)), atan(y, x), s);
 }
 
@@ -323,37 +341,37 @@ float fmod(float x, float y)
 
 float rounded_polygon(vec2 uv, float width, float height, float sides, float roundness)
 {
-    float(PI) = 3.14159;
-    float(HALF_PI) = 1.5708;
+    const float PI = 3.14159;
+    float HALF_PI = 1.5708;
     uv = ((uv * 2.0) + vec2((- 1.0), (- 1.0)));
-    float(epsilon) = 0;
+    float epsilon = 0;
     uv.x = (uv.x / (width + (((width == 0) ? 1 : 0) * epsilon)));
     uv.y = (uv.y / (height + (((height == 0) ? 1 : 0) * epsilon)));
     roundness = clamp(roundness, 0.0, 1.0);
-    float(i_sides) = floor(abs(sides));
-    float(fullAngle) = ((2.0 * PI) / i_sides);
-    float(halfAngle) = (fullAngle / 2.0);
-    float(opositeAngle) = (HALF_PI - halfAngle);
-    float(diagonal) = (1.0 / cos(halfAngle));
-    float(chamferAngle) = (roundness * halfAngle);
-    float(remainingAngle) = (halfAngle - chamferAngle);
-    float(ratio) = (tan(remainingAngle) / tan(halfAngle));
-    vec2(chamferCenter) = ((vec2(cos(halfAngle), sin(halfAngle)) * ratio) * diagonal);
-    vec2(chamferOrigin) = vec2(1.0, tan(remainingAngle));
-    float(distA) = length(chamferCenter);
-    float(distB) = (1.0 - chamferCenter.x);
-    float(distCref) = length(chamferOrigin);
-    float(uvScale) = diagonal;
+    float i_sides = floor(abs(sides));
+    float fullAngle = ((2.0 * PI) / i_sides);
+    float halfAngle = (fullAngle / 2.0);
+    float opositeAngle = (HALF_PI - halfAngle);
+    float diagonal = (1.0 / cos(halfAngle));
+    float chamferAngle = (roundness * halfAngle);
+    float remainingAngle = (halfAngle - chamferAngle);
+    float ratio = (tan(remainingAngle) / tan(halfAngle));
+    vec2 chamferCenter = ((vec2(cos(halfAngle), sin(halfAngle)) * ratio) * diagonal);
+    vec2 chamferOrigin = vec2(1.0, tan(remainingAngle));
+    float distA = length(chamferCenter);
+    float distB = (1.0 - chamferCenter.x);
+    float distCref = length(chamferOrigin);
+    float uvScale = diagonal;
     uv *= uvScale;
-    vec2(polaruv) = vec2(atan2(uv.y, uv.x), length(uv));
+    vec2 polaruv = vec2(atan2(uv.y, uv.x), length(uv));
     polaruv.x += (HALF_PI + (2 * PI));
     polaruv.x = fmod((polaruv.x + halfAngle), fullAngle);
     polaruv.x = abs((polaruv.x - halfAngle));
     uv = (vec2(cos(polaruv.x), sin(polaruv.x)) * polaruv.y);
-    float(angleRatio) = (1.0 - ((polaruv.x - remainingAngle) / chamferAngle));
-    float(distC) = sqrt((((distA * distA) + (distB * distB)) - (((2.0 * distA) * distB) * cos((PI - (halfAngle * angleRatio))))));
-    float(ret) = uv.x;
-    float(chamferZone) = (((halfAngle - polaruv.x) < chamferAngle) ? 1 : 0);
+    float angleRatio = (1.0 - ((polaruv.x - remainingAngle) / chamferAngle));
+    float distC = sqrt((((distA * distA) + (distB * distB)) - (((2.0 * distA) * distB) * cos((PI - (halfAngle * angleRatio))))));
+    float ret = uv.x;
+    float chamferZone = (((halfAngle - polaruv.x) < chamferAngle) ? 1 : 0);
     ret = mix(uv.x, (polaruv.y / distC), chamferZone);
     ret = clamp(((1 - ret) / fwidth(ret)), 0.0, 1.0);
     return ret;
@@ -520,15 +538,10 @@ float worley_no_cells(vec2 st, float frequency)
 
     REQUIRE(ss.str() == R"(
 uniform float u_seed;
-
 uniform int u_dist_op;
-
 uniform int u_features_mode;
-
 uniform float u_manhattan_scale;
-
 uniform vec2 u_poisson_random_offset;
-
 vec2 _random2(vec2 p)
 {
     return fract((sin(vec2(dot(p, vec2(127.1, 311.7)), dot(p, vec2(269.5, 183.3)))) * 43758.5));
@@ -546,25 +559,25 @@ float _manhattan_dist(vec2 diff)
 
 float _chebyshev_dist(vec2 diff)
 {
-    float(dx) = abs(diff.x);
-    float(dy) = abs(diff.y);
+    float dx = abs(diff.x);
+    float dy = abs(diff.y);
     return max(dx, dy);
 }
 
 float worley_cells(vec2 st)
 {
-    vec2(i_st) = floor(st);
-    vec2(f_st) = fract(st);
-    float(f1) = 1.0, f2 = 1.0;
-    for (int(y) = (- 1); (y <= 1); y++)
+    vec2 i_st = floor(st);
+    vec2 f_st = fract(st);
+    float f1 = 1.0, f2 = 1.0;
+    for (int y = (- 1); (y <= 1); y++)
     {
-        for (int(x) = (- 1); (x <= 1); x++)
+        for (int x = (- 1); (x <= 1); x++)
         {
-            vec2(neighbor) = vec2(float(x), float(y));
-            vec2(point) = _random2((i_st + neighbor));
+            vec2 neighbor = vec2(float(x), float(y));
+            vec2 point = _random2((i_st + neighbor));
             point = (0.5 + (0.5 * sin((u_seed + (6.2831 * point)))));
-            vec2(diff) = ((neighbor + point) - f_st);
-            float(dist) = 0;
+            vec2 diff = ((neighbor + point) - f_st);
+            float dist = 0;
             if ((u_dist_op == 0)) {
                 dist = _euclidean_dist(diff);
             } else if ((u_dist_op == 1)) {
@@ -599,20 +612,20 @@ float worley_cells(vec2 st)
 
 float worley_no_cells(vec2 st, float frequency)
 {
-    uint(s) = poisson_morton(uint(u_poisson_random_offset.x), uint(u_poisson_random_offset.y));
+    uint s = poisson_morton(uint(u_poisson_random_offset.x), uint(u_poisson_random_offset.y));
     if ((s == 0)) {
         s = 1;
     }
     poisson_seeding(s);
-    float(min_dist) = 1;
-    float(density) = 1.5;
-    uint(num) = poisson_number(((frequency * frequency) * density));
-    for (uint(i) = 0; (i < num); (++ i))
+    float min_dist = 1;
+    float density = 1.5;
+    uint num = poisson_number(((frequency * frequency) * density));
+    for (uint i = 0; (i < num); (++ i))
     {
-        float(x) = poisson_uniform_0_1();
-        float(y) = poisson_uniform_0_1();
-        vec2(diff) = ((vec2(x, y) * frequency) - st);
-        float(dist) = 0;
+        float x = poisson_uniform_0_1();
+        float y = poisson_uniform_0_1();
+        vec2 diff = ((vec2(x, y) * frequency) - st);
+        float dist = 0;
         if ((u_dist_op == 0)) {
             dist = _euclidean_dist(diff);
         } else if ((u_dist_op == 1)) {
@@ -653,7 +666,7 @@ void test() {
     REQUIRE(ss.str() == R"(
 void test()
 {
-    int(i) = 0;
+    int i = 0;
     while (true)
     {
     }
@@ -689,9 +702,9 @@ void main() {
     REQUIRE(ss.str() == R"(
 void main()
 {
-    float(a) = 0, b = 0, c = 0, d = 0;
-    float(w) = (a ? a, b : b), c;
-    float(z) = (a ? (b ? c : d) : w);
+    float a = 0, b = 0, c = 0, d = 0;
+    float w = (a ? a, b : b), c;
+    float z = (a ? (b ? c : d) : w);
 }
 
 )"+1);
@@ -737,7 +750,7 @@ void test() {
     REQUIRE(ss.str() == R"(
 void test()
 {
-    int(i) = 10;
+    int i = 10;
     switch (i)
     {
         case 0:
@@ -843,17 +856,17 @@ void main() {
     REQUIRE(ss.str() == R"(
 void main()
 {
-    int(i) = 0;
+    int i = 0;
     for (; ; )
     {
     }
     for (i = 0; ; )
     {
     }
-    for (int(i) = 0; (i < 10); )
+    for (int i = 0; (i < 10); )
     {
     }
-    for (int(i) = 0; (i < 10); i++)
+    for (int i = 0; (i < 10); i++)
     {
     }
 }
